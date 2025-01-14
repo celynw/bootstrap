2b642207db922169f7ac56015e67a0c4b777a712
Celyn Walters <3299161+celynw@users.noreply.github.com>
2025-01-14 18:26:58 +0000

    Use python virtual environment with uv for PEP668

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,12 @@
 set -e
 
 sudo apt update
-sudo apt install -y python3-pip
-pip install ansible
+sudo apt install -y curl
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv venv
+source ./.venv/bin/activate
+uv pip install ansible
 
 mkdir -p /tmp/ansible-setup
 wget -O /tmp/ansible-setup/initialise.yml https://raw.githubusercontent.com/celynw/bootstrap/master/bootstrap.yml
