c1754ee1d419ab11afb91c3410d37c57d4a17aeb
Celyn Walters <3299161+celynw@users.noreply.github.com>
2025-01-14 18:31:23 +0000

    Use `.` over `source` for compatibility with `sh`

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ set -e
 sudo apt update
 sudo apt install -y curl
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+. $HOME/.local/bin/env
 uv venv
-source ./.venv/bin/activate
+. ./.venv/bin/activate
 uv pip install ansible
 
 mkdir -p /tmp/ansible-setup
